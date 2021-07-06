b936eeca51e758ae3464f2ecd6785dde2a503369
Abhirup <abhirupdatta4@gmail.com>
2021-07-06 10:34:43 +0530

    Output/Video file not incoming

--- lane-detection.py
+++ lane-detection.py
@@ -1,16 +1,7 @@
-import matplotlib.pylab as plt
+#import matplotlib.pylab as plt
 import numpy as np
 import cv2
 
-img=cv2.imread("Road.jpg")
-img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-
-print(img.shape)
-h=img.shape[0]
-w=img.shape[1]
-
-
-roi_v=[(0,h),(w/2,(h+100)/2),(w,h)]
 
 def roi(img,v):
     mask=np.zeros_like(img)
@@ -25,18 +16,47 @@ def linedraw(img,lines):
     lineimg=np.zeros((img.shape[0],img.shape[1],3),dtype=np.uint8)
     for l in lines:
         for x1,y1,x2,y2 in l:
-            cv2.line(lineimg,(x1,y1),(x2,y2),(100,255,0),thickness=5)
+            cv2.line(lineimg,(x1,y1),(x2,y2),(0,255,0),thickness=5)
             
     img=cv2.addWeighted(img,0.8,lineimg,1,0.0)
     return img
 
-grayimg=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-canimg=cv2.Canny(grayimg,100,200)
-crop=roi(canimg,np.array([roi_v],np.int32),)
-lines=cv2.HoughLinesP(crop, rho=6, theta=np.pi/60, threshold=160 ,lines=np.array([]),minLineLength=40,maxLineGap=25)
+#img=cv2.imread("Road.jpg")
+#img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+def process(image):
+    print(image.shape)
+    h=image.shape[0]
+    w=image.shape[1]
+    roi_v=[(0,h),(w/2,h/2),(w,h)]
+    grayimg=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    canimg=cv2.Canny(grayimg,100,200)
+    crop=roi(canimg,np.array([roi_v],np.int32),)
+    lines=cv2.HoughLinesP(crop, rho=6, theta=np.pi/60, threshold=160 ,lines=np.array([]),minLineLength=40,maxLineGap=25)
+    imgwithlines=linedraw(image, lines)
+    if lines is None:
+        imagewithlines = image
+    else:
+        imagewithlines = linedraw(image, lines)
+    return imgwithlines
+
+
+cap=cv2.VideoCapture("Moving Road.mp4")
+while(cap.isOpened()):
+    ret,frame=cap.read()
+    frame=process(frame)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
 
 
-imgwithlines=linedraw(img, lines)
-plt.imshow(imgwithlines)
-plt.show() 
+
+
+
+
+
+
+ 
